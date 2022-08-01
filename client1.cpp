aae22c1a24805f87b307334dd82019f63afe7908
Harry Feng <109060049+hf-harryfeng@users.noreply.github.com>
2022-08-01 15:19:57 +0800

    Update client1.cpp

--- client1.cpp
+++ client1.cpp
@@ -29,17 +29,14 @@ int main(int argc, char **argv)
 
     struct sockaddr_in ser;
     ser.sin_family = AF_INET;
-    ser.sin_port = htons(atoi("8080"));
-    ser.sin_addr.s_addr = inet_addr("172.20.38.143");
+    ser.sin_port = htons(atoi("8888"));
+    ser.sin_addr.s_addr = inet_addr("172.20.38.142");
 
     if (connect(sockfd, (struct sockaddr *)&ser, len) < 0)
     {
-        perror("bind error");
+        perror("connect error");
         exit(EXIT_FAILURE);
     }
-
-    struct epoll_event events[MAX_EVENTS];
-    struct epoll_event ev;
 
     char *l_opt_arg;
     const char *short_options = "g:p:";
@@ -53,19 +50,19 @@ int main(int argc, char **argv)
     {
         switch (C)
         {
-            l_opt_arg = optarg;
+            l_opt_arg = optarg; /*l_opt_arg指向-g或者-p后所跟参数（文件名）*/
         case 'g':
-            ret = send(sockfd, l_opt_arg, sizeof(l_opt_arg), 0);
-            fp = open(l_opt_arg, O_WRONLY | O_CREAT, 0664);
+            ret = send(sockfd, l_opt_arg, sizeof(l_opt_arg), 0); /*发送要获取的文件名*/
+            fp = open(l_opt_arg, O_WRONLY | O_CREAT, 0664);      /*创建一个需要get的新文件*/
             if (fp < 0)
             {
                 printf("File :\t%s can not open to write\n", l_opt_arg);
                 exit(1);
             }
 
-            ret2 = recv(sockfd, buf, sizeof(buf), 0);
-            while (ret2 > 0)
+            while (ret2 > 0) /*循环接受数据并写入文件*/
             {
+                ret2 = recv(sockfd, buf, sizeof(buf), 0);
                 if (write(fp, buf, sizeof(buf)) == -1)
                 {
                     printf("File :%s\t write failed\n", l_opt_arg);
@@ -76,15 +73,16 @@ int main(int argc, char **argv)
             printf("this file: %s\t get success", l_opt_arg);
             break;
         case 'p':
-            fp = open(l_opt_arg, O_RDONLY);
+            fp = open(l_opt_arg, O_RDONLY); /*打开要发送的文件*/
             if (fp < 0)
             {
                 printf("File :%s\t can not open to write\n", l_opt_arg);
                 exit(1);
             }
-            ret3 = fread(buf, sizeof(char), sizeof(buf), 0);
-            while (ret3 > 0)
+
+            while (ret3 > 0) /*循环读取文件并发送*/
             {
+                ret3 = fread(buf, sizeof(char), sizeof(buf), 0);
                 if (send(sockfd, buf, sizeof(buf), 0) < 0)
                 {
                     printf("File:%s\t read Failed\n", l_opt_arg);

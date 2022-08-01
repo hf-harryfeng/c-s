d9dc6a846eed61f3d4f0fc6318be2c3a7603360a
Harry Feng <109060049+hf-harryfeng@users.noreply.github.com>
2022-08-01 15:21:02 +0800

    Update server1.cpp

--- server1.cpp
+++ server1.cpp
@@ -23,48 +23,53 @@
 #define MAX_EVENTS 10
 
 typedef unsigned long int unit64_t;
-int createsocket(int sockfd);
-
-int bindsocket(int sockfd, struct sockaddr *addr, socklen_t addrlen);
-
-int listensocket(int sockfd, int max_events);
-
-int acceptsocket(int sockfd, struct sockaddr *addr, socklen_t *addrlen);
 
 void cf(io_context_t ctx, struct iocb *iocb, long res, long res2);
 
 struct epoll_event *events;
-struct epoll_event ev;
+struct epoll_event ev; /*定义epoll对象*/
 struct io_event eve[64] = {};
 
 int main(int argc, char *argv[])
 {
   int evfd, fp;
   int sockfd;
-  io_context_t ctx;
-  char recvname[20];
-  char *buf;
-  struct iocb cb;
-  struct iocb *pcbs[1];
-  struct timespec ts;
+  io_context_t ctx; /*异步io fd*/
+  char buf[1024] = {0};
+  char filename[10] = "test.txt";
+  struct iocb cb;       /*一个异步io读取文件事件*/
+  struct iocb *pcbs[1]; /*io_submit参数，包含cb*/
+  struct timespec ts;   /*io_getevents定义定时器*/
   int n;
-  unit64_t ready;
-  struct io_event *events_ret;
+  unit64_t ready;              /*已完成的异步io事件*/
+  struct io_event *events_ret; /*io_getevents参数*/
 
   struct sockaddr_in ser;
   ser.sin_family = AF_INET;
   ser.sin_port = htons(atoi("8888"));
-  ser.sin_addr.s_addr = inet_addr("172.20.38.143");
-
-  createsocket(sockfd);
+  ser.sin_addr.s_addr = inet_addr("172.20.38.142");
+  /*绑定通讯地址*/
+  sockfd = socket(AF_INET, SOCK_STREAM, 0); /*创建socket套接字*/
+  if (sockfd < 0)
+  {
+    perror("sockfd error");
+    return -1;
+  }
   socklen_t len = sizeof(struct sockaddr_in);
-  bindsocket(sockfd, (struct sockaddr *)&ser, len);
-  listensocket(sockfd, MAX_EVENTS);
-  acceptsocket(sockfd, (struct sockaddr *)&ser, &len);
-  int epoll_fd = epoll_create(MAX_EVENTS);
-  ev.events = EPOLLIN;
+  if (bind(sockfd, (struct sockaddr *)&ser, len) < 0) /*绑定套接字地址*/
+  {
+    perror("bind error");
+    exit(EXIT_FAILURE);
+  }
+  if (listen(sockfd, MAX_EVENTS) < 0) /*监听socket套接字，MAX_EVENTS监听最大数*/
+  {
+    perror("listen error");
+    exit(EXIT_FAILURE);
+  }
+  int epoll_fd = epoll_create(MAX_EVENTS); /*初始化epoll*/
+  ev.events = EPOLLIN;                     /*epoll 事件为可读*/
   ev.data.fd = sockfd;
-  int ret1 = epoll_ctl(epoll_fd, EPOLL_CTL_ADD, sockfd, &ev);
+  int ret1 = epoll_ctl(epoll_fd, EPOLL_CTL_ADD, sockfd, &ev); /*添加ev.data.fd对应的事件进表*/
   if (ret1 == -1)
   {
     perror("epoll_ctl  sockfd error");
@@ -81,103 +86,94 @@ int main(int argc, char *argv[])
     }
     ev.events = EPOLLIN | EPOLLET;
     ev.data.ptr = NULL;
-    if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, evfd, &ev) != 0)
+    if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, evfd, &ev) != 0) /*初始化eventfd描述符，并将其添加进epoll*/
     {
       perror("evfd epoll_ctl error");
       exit(6);
     }
-    ctx = 0;
-    memset(&ctx,0,sizeof(ctx))
-    int errcode = io_setup(1024,&ctx);
-    if(errcode != 0)
+    ctx = 0; /*初始化异步io上下文*/
+    memset(&ctx, 0, sizeof(ctx));
+    int errcode = io_setup(1024, &ctx);
+    if (errcode != 0)
     {
       perror("open");
       exit(7);
     }
-    int nfds = epoll_wait(epoll_fd, events, MAX_EVENTS, -1);
-    if (nfds == -1)
+    int nfds = epoll_wait(epoll_fd, events, MAX_EVENTS, -1); /*调用epoll_wait等待事件发生*/
+    if (nfds <= 0)
     {
-      perror("epoll_wait error");
-      exit(EXIT_FAILURE);
+      if (errno != EINTR)
+      {
+        perror("epoll_wait error");
+        exit(8);
+      }
     }
+    else
+      continue;
     printf("nfds:%d\n", nfds);
 
     for (int i = 0; i < nfds; ++i)
     {
-      if (events[i].data.fd == sockfd)
+      if (events[i].data.fd == sockfd) /*如果有新的sockfd连接*/
       {
-        int confd = acceptsocket(sockfd, (struct sockaddr *)&ser, &len);
+        int confd = accept(sockfd, (struct sockaddr *)&ser, &len);
         ev.events = EPOLLIN | EPOLLET;
         ev.data.fd = confd;
-        int ret2 = epoll_ctl(epoll_fd, EPOLL_CTL_ADD, confd, &ev);
+        int ret2 = epoll_ctl(epoll_fd, EPOLL_CTL_ADD, confd, &ev); /*初始化accept进行接受，并将其添加进epoll事件*/
         if (ret2 == -1)
         {
           perror("epoll_ctl  sockfd error");
           return -1;
         }
       }
-      else if (events[i].events & EPOLLIN)
+      else if (events[i].events & EPOLLIN) /*如果是已经连接的用户，并且接收数据，那么进行读入*/
       {
-        int r_fd = recv(sockfd, buf, 0, sizeof(buf));
-        if (r_fd == -1)
-        {
-          perror("recv error");
-          return -1;
-        }
-        if (r_fd == 0)
-        {
-          perror("退出");
-          ev.events = EPOLLIN;
-          ev.data.fd = events[i].data.fd;
-          epoll_ctl(epoll_fd, EPOLL_CTL_DEL, ev.data.fd, &ev);
-          close(ev.data.fd);
-        }
-        else
-        {
-          strncpy(recvname, buf, strlen(buf));
-          fp = open(buf, O_WRONLY, 0664);
-          errcode = posix_memalign((void**)&buf,512,1024);
-          io_prep_pwrite(&cb, fp, buf, 1024, 0);
-          io_set_eventfd(&cb, evfd);
-          io_set_callback(&cb, cf);
-          pcbs[0] = &cb;
-          if (io_submit(ctx, 1, pcbs) != 1)
-          {
-            perror("io_submit");
-            exit(8);
-          }
-          send(events[i].data.fd, buf, sizeof(buf), 0);
-        }
-      }
-      else if (events[i].events & EPOLLOUT)
-      {
-        fp = open(buf, O_RDONLY | O_CREAT, 0664);
-        errcode = posix_memalign((void**)&buf,512,1024);
-        io_prep_pread(&cb, fp, buf, 1024, 0);
-        io_set_eventfd(&cb, evfd);
+        fp = open(filename, O_RDONLY | O_CREAT, 0664); /*创建接收的新文件*/
+        io_prep_pread(&cb, fp, buf, 1024, 0);          /*将buf内容异步读入fp指向文件请求*/
+        io_set_eventfd(&cb, evfd);                     /*将eventfd与cb上下文进行绑定*/
         io_set_callback(&cb, cf);
         pcbs[0] = &cb;
-        if (io_submit(ctx, 1, pcbs) != 1)
+        if (io_submit(ctx, 1, pcbs) != 1) /*异步请求处理*/
         {
           perror("io_submit");
-          exit(11);
+          exit(10);
         }
         n = read(evfd, &ready, sizeof(ready));
         if (n != 8)
         {
           perror("read error");
-          exit(12);
+          exit(11);
         }
-        ts.tv_sec = 0;
-        ts.tv_nsec = 0;
-        events_ret = (struct io_event *)malloc(sizeof(struct io_event) * 32);
-        n = io_getevents(ctx, 1, 32, events_ret, &ts);
-
-        printf("log: %lu events are ready, get %d events\n", ready, n);
-
-        ((io_callback_t)(events_ret[0].data))(ctx, events_ret[0].obj, events_ret[0].res, events_ret[0].res2);
+        memset(&buf, 0x00, 1023);
       }
-      memset(&buf, 0x00, 1023);
+      else if (events[i].events & EPOLLOUT) /*如果是已经连接的用户，并且有数据发送*/
+      {
+        int r_fd = recv(sockfd, buf, 0, sizeof(recvname)); /*接收客户端发送的文件名*/
+
+        fp = open(buf, O_WRONLY, 0664); /*打开要发送的文件*/
+        memset(&buf, 0x00, 1023);
+        io_prep_pwrite(&cb, fp, buf, 1024, 0); /*将fp指向文件内容异步写入buf请求*/
+        io_set_eventfd(&cb, evfd);             /*将eventfd与cb上下文进行绑定*/
+        io_set_callback(&cb, cf);
+        pcbs[0] = &cb;
+        if (io_submit(ctx, 1, pcbs) != 1) /*异步请求处理*/
+        {
+          perror("io_submit");
+          exit(9);
+        }
+        send(sockfd, buf, sizeof(buf), 0);
+        ev.data.fd = sockfd;
+        ev.events = EPOLLIN | EPOLLET;
+        epoll_ctl(epoll_fd, EPOLL_CTL_MOD, sockfd, &ev);
+      }
+      ts.tv_sec = 0;    /*获取异步请求处理结果计时器*/
+      ts.tv_nsec = 0;
+      events_ret = (struct io_event *)malloc(sizeof(struct io_event) * 32);
+      n = io_getevents(ctx, 1, 32, events_ret, &ts);/*获取异步请求处理结果*/
+
+      printf("log: %lu events are ready, get %d events\n", ready, n);
+
+      ((io_callback_t)(events_ret[0].data))(ctx, events_ret[0].obj, events_ret[0].res, events_ret[0].res2);
     }
   }
   close(epoll_fd);
@@ -189,45 +185,8 @@ int main(int argc, char *argv[])
   close(sockfd);
 }
 
-int createsocket(int sockfd)
-{
-  sockfd = socket(AF_INET, SOCK_STREAM, 0);
-  if (sockfd < 0)
-  {
-    perror("sockfd error");
-    return -1;
-  }
-}
-
-int bindsocket(int sockfd, struct sockaddr *addr, socklen_t addrlen)
-{
-  if (bind(sockfd, (struct sockaddr *)&addr, addrlen) < 0)
-  {
-    perror("bind error");
-    exit(EXIT_FAILURE);
-  }
-}
-
-int listensocket(int sockfd, int max_events)
-{
-  if (listen(sockfd, max_events) < 0)
-  {
-    perror("listen error");
-    exit(EXIT_FAILURE);
-  }
-}
-
-int acceptsocket(int sockfd, struct sockaddr *addr, socklen_t *addrlen)
-{
-  if (accept(sockfd, (struct sockaddr *)addr, addrlen) < 0)
-  {
-    perror("accept error");
-    exit(EXIT_FAILURE);
-  }
-}
-
 void cf(io_context_t ctx, struct iocb *iocb, long res, long res2)
 {
   printf("can read %lu bytes, and in fact has read %ld bytes\n", iocb->u.c.nbytes, res);
-  printf("the content is :\n%lu", iocb->u.c.buf);
+  printf("the content is :%lu", iocb->u.c.buf);
 }
